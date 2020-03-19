a0f1d70b4b01a81a642b77805fc054598e3513ad
Armando Pesenti Gritti <arpesenti@gmail.com>
2020-03-19 12:23:52 +0000

    Add stubs for basic NSURLSession classes and methods

--- Headers/Foundation/NSURLSession.h
+++ Headers/Foundation/NSURLSession.h
@@ -7,13 +7,166 @@
 @protocol NSURLSessionDelegate;
 @protocol NSURLSessionTaskDelegate;
 
+@class NSURLSessionConfiguration;
+@class NSOperationQueue;
+@class NSURLSessionDataTask;
+@class NSURL;
+@class NSURLRequest;
+@class NSURLResponse;
+@class NSError;
+
 @interface NSURLSession : NSObject
+{
+  NSOperationQueue           *_delegateQueue;
+  id <NSURLSessionDelegate>  _delegate;
+  NSURLSessionConfiguration  *_configuration;
+  NSString                   *_sessionDescription;
+}
+
+/*
+ * Customization of NSURLSession occurs during creation of a new session.
+ * If you do specify a delegate, the delegate will be retained until after
+ * the delegate has been sent the URLSession:didBecomeInvalidWithError: message.
+ */
++ (NSURLSession*) sessionWithConfiguration: (NSURLSessionConfiguration*)configuration 
+                                  delegate: (id <NSURLSessionDelegate>)delegate 
+                             delegateQueue: (NSOperationQueue*)queue;
+
+- (NSOperationQueue*) delegateQueue;
+
+- (id <NSURLSessionDelegate>) delegate;
+
+- (NSURLSessionConfiguration*) configuration;
+
+- (NSString*) sessionDescription;
+
+- (void) setSessionDescription: (NSString*)sessionDescription;
+
+/* -finishTasksAndInvalidate returns immediately and existing tasks will be 
+ * allowed to run to completion.  New tasks may not be created.  The session
+ * will continue to make delegate callbacks until 
+ * URLSession:didBecomeInvalidWithError: has been issued. 
+ *
+ * When invalidating a background session, it is not safe to create another 
+ * background session with the same identifier until 
+ * URLSession:didBecomeInvalidWithError: has been issued.
+ */
+- (void) finishTasksAndInvalidate;
+
+/* -invalidateAndCancel acts as -finishTasksAndInvalidate, but issues
+ * -cancel to all outstanding tasks for this session.  Note task 
+ * cancellation is subject to the state of the task, and some tasks may
+ * have already have completed at the time they are sent -cancel. 
+ */
+- (void) invalidateAndCancel;
+
+/* 
+ * NSURLSessionTask objects are always created in a suspended state and
+ * must be sent the -resume message before they will execute.
+ */
+
+/* Creates a data task with the given request. 
+ * The request may have a body stream. */
+- (NSURLSessionDataTask*) dataTaskWithRequest: (NSURLRequest*)request;
+
+/* Creates a data task to retrieve the contents of the given URL. */
+- (NSURLSessionDataTask*) dataTaskWithURL: (NSURL*)url;
+
 @end
 
-@interface NSURLSessionConfiguration : NSObject <NSCopying>
-@end
+typedef NS_ENUM(NSUInteger, NSURLSessionTaskState) {
+  /* The task is currently being serviced by the session */
+  NSURLSessionTaskStateRunning = 0,    
+  NSURLSessionTaskStateSuspended = 1,
+  /* The task has been told to cancel.  
+   * The session will receive URLSession:task:didCompleteWithError:. */
+  NSURLSessionTaskStateCanceling = 2,  
+  /* The task has completed and the session will receive no more 
+   * delegate notifications */
+  NSURLSessionTaskStateCompleted = 3,  
+};
 
+/*
+ * NSURLSessionTask - a cancelable object that refers to the lifetime
+ * of processing a given request.
+ */
 @interface NSURLSessionTask : NSObject <NSCopying>
+{
+  NSUInteger    _taskIdentifier;                /* an identifier for this task, 
+                                                 * assigned by and unique to the 
+                                                 * owning session */
+  NSURLRequest  *_originalRequest;
+  NSURLRequest  *_currentRequest;               /* may differ from 
+                                                 * originalRequest due to http 
+                                                 * server redirection */
+  NSURLResponse *_response;                     /* may be nil if no response has
+                                                 * been received */
+  int64_t       _countOfBytesReceived;          /* number of body bytes already 
+                                                 * received */
+  int64_t       _countOfBytesSent;              /* number of body bytes already 
+                                                 * sent */
+  int64_t       _countOfBytesExpectedToSend;    /* number of body bytes we 
+                                                 * expect to send, derived from 
+                                                 * the Content-Length of the 
+                                                 * HTTP request */
+  int64_t       _countOfBytesExpectedToReceive; /* number of byte bytes we 
+                                                 * expect to receive, usually 
+                                                 * derived from the 
+                                                 * Content-Length header of an 
+                                                 * HTTP response. */
+  NSString      *_taskDescription;
+  NSURLSessionTaskState _state;                 /* The current state of the task 
+                                                 * within the session. */
+  NSError       *_error;                        /* The error, if any, delivered 
+                                                 * via -URLSession:task:didCompleteWithError:
+                                                 * This property will be nil in 
+                                                 * the event that no error 
+                                                 * occured. */
+}
+
+- (NSUInteger) taskIdentifier;
+
+- (NSURLRequest*) originalRequest;
+
+- (NSURLRequest*) currentRequest;
+
+- (NSURLResponse*) response;
+
+- (int64_t) countOfBytesReceived;
+
+- (int64_t) countOfBytesSent;
+
+- (int64_t) countOfBytesExpectedToSend;
+
+- (int64_t) countOfBytesExpectedToReceive;
+
+- (NSString*) taskDescription;
+
+- (void) setTaskDescription: (NSString*)taskDescription;
+
+- (NSURLSessionTaskState) state;
+
+- (NSError*) error;
+
+/* -cancel returns immediately, but marks a task as being canceled.
+ * The task will signal -URLSession:task:didCompleteWithError: with an
+ * error value of { NSURLErrorDomain, NSURLErrorCancelled }. In some 
+ * cases, the task may signal other work before it acknowledges the 
+ * cancelation.  -cancel may be sent to a task that has been suspended.
+ */
+- (void) cancel;
+
+/*
+ * Suspending a task will prevent the NSURLSession from continuing to
+ * load data.  There may still be delegate calls made on behalf of
+ * this task (for instance, to report data received while suspending)
+ * but no further transmissions will be made on behalf of the task
+ * until -resume is sent.  The timeout timer associated with the task
+ * will be disabled while a task is suspended.
+ */
+- (void) suspend;
+- (void) resume;
+
 @end
 
 @interface NSURLSessionDataTask : NSURLSessionTask
@@ -30,10 +183,44 @@
 @end
 #endif
 
+/*
+ * Configuration options for an NSURLSession.  When a session is
+ * created, a copy of the configuration object is made - you cannot
+ * modify the configuration of a session after it has been created.
+ */
+@interface NSURLSessionConfiguration : NSObject <NSCopying>
+@end
+
 @protocol NSURLSessionDelegate <NSObject>
+
+/* The last message a session receives.  A session will only become
+ * invalid because of a systemic error or when it has been
+ * explicitly invalidated, in which case the error parameter will be nil.
+ */
+- (void)         URLSession: (NSURLSession*)session 
+  didBecomeInvalidWithError: (NSError*)error;
+
 @end
 
 @protocol NSURLSessionTaskDelegate <NSURLSessionDelegate>
+
+/* Sent as the last message related to a specific task.  Error may be
+ * nil, which implies that no error occurred and this task is complete. 
+ */
+- (void )   URLSession: (NSURLSession*)session 
+                  task: (NSURLSessionTask*)task
+  didCompleteWithError: (NSError*)error;
+
+@end
+
+@protocol NSURLSessionDataDelegate <NSURLSessionTaskDelegate>
+
+/* Sent when data is available for the delegate to consume.
+ */
+- (void) URLSession: (NSURLSession*)session 
+           dataTask: (NSURLSessionDataTask*)dataTask
+     didReceiveData: (NSData*)data;
+
 @end
 
 #endif
--- Source/NSURLSession.m
+++ Source/NSURLSession.m
@@ -1,4 +1,165 @@
 #import <Foundation/NSURLSession.h>
 
 @implementation NSURLSession
+
++ (NSURLSession *) sessionWithConfiguration: (NSURLSessionConfiguration*)configuration 
+                                   delegate: (id <NSURLSessionDelegate>)delegate 
+                              delegateQueue: (NSOperationQueue*)queue
+{
+  //TODO
+  return nil;
+}
+
+- (NSOperationQueue*) delegateQueue
+{
+  return _delegateQueue;
+}
+
+- (id <NSURLSessionDelegate>) delegate
+{
+  return _delegate;
+}
+
+- (NSURLSessionConfiguration*) configuration
+{
+  return _configuration;
+}
+
+- (NSString*) sessionDescription
+{
+  return _sessionDescription;
+}
+
+- (void) setSessionDescription: (NSString*)sessionDescription
+{
+  ASSIGN(_sessionDescription, sessionDescription);
+}
+
+- (void) finishTasksAndInvalidate
+{
+  //TODO
+}
+
+- (void) invalidateAndCancel
+{
+  //TODO
+}
+
+- (NSURLSessionDataTask*) dataTaskWithRequest: (NSURLRequest*)request
+{
+  //TODO
+  return nil;
+}
+
+- (NSURLSessionDataTask*) dataTaskWithURL: (NSURL*)url
+{
+  //TODO
+  return nil;
+}
+
+- (id) copyWithZone: (NSZone*)zone
+{
+  //TODO
+  return self;
+}
+
+@end
+
+@implementation NSURLSessionTask
+
+- (NSUInteger) taskIdentifier
+{
+  return _taskIdentifier;
+}
+
+- (NSURLRequest*) originalRequest
+{
+  return _originalRequest;
+}
+
+- (NSURLRequest*) currentRequest
+{
+  return _currentRequest;
+}
+
+- (NSURLResponse*) response
+{
+  return _response;
+}
+
+- (int64_t) countOfBytesReceived
+{
+  return _countOfBytesReceived;
+}
+
+- (int64_t) countOfBytesSent
+{
+  return _countOfBytesSent;
+}
+
+- (int64_t) countOfBytesExpectedToSend
+{
+  return _countOfBytesExpectedToSend;
+}
+
+- (int64_t) countOfBytesExpectedToReceive
+{
+  return _countOfBytesExpectedToReceive;
+}
+
+- (NSString*) taskDescription
+{
+  return _taskDescription;
+}
+
+- (void) setTaskDescription: (NSString*)taskDescription
+{
+  ASSIGN(_taskDescription, taskDescription);
+}
+
+- (NSURLSessionTaskState) state
+{
+  return _state;
+}
+
+- (NSError*) error
+{
+  return _error;
+}
+
+- (void) cancel
+{
+  //TODO
+}
+
+- (void) suspend
+{
+  //TODO
+}
+
+- (void) resume
+{
+  //TODO
+}
+
+- (id) copyWithZone: (NSZone*)zone
+{
+  //TODO
+  return self;
+}
+
+@end
+
+@implementation NSURLSessionDataTask
+
+@end
+
+@implementation NSURLSessionConfiguration
+
+- (id) copyWithZone: (NSZone*)zone
+{
+  //TODO
+  return self;
+}
+
 @end
